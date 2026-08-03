10ac91f86945da72a2d731b5068eb3ce40fc89ba
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-03 13:59:54 +0530

    constructar parametrized done

---
+++ Employee.java
@@ -0,0 +1,9 @@
+class Employee{
+    Employee(String name, int salary){
+        System.out.println("employee name: " + name);
+        System.out.println("employee salary: " + salary);
+    }
+    public static void main(String[] args){
+        Employee e = new Employee("shweta", 20000);
+    }
+}
