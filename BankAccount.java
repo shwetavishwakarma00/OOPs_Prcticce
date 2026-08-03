1c142fd3f3a2310634b261e53361eef1bf164006
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-03 15:53:26 +0530

    question 4 is done

---
+++ BankAccount.java
@@ -0,0 +1,19 @@
+
+class BankAccount {
+
+    private int balance;
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        BankAccount ac = new BankAccount();
+        ac.balance = 6000;
+        System.out.println("Balance: " + ac.getBalance());
+    }
+}
