ebfaa6def49941bafada01c282d4cac49a0c2f8d
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-03 16:01:38 +0530

    qution 5th is done

--- Car.java
+++
@@ -1,8 +0,0 @@
-class Car{
-    Car(){
-        System.out.println("Car Object Created");
-    }
-    public static void main(String[] args){
-        Car c = new Car();    //constructor is called when object is created
-    }
-}
