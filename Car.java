9a2ea3bc909ad36b4f02e3cc124e38fbbff1355f
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-03 13:48:57 +0530

    question 2 is done

---
+++ Car.java
@@ -0,0 +1,8 @@
+class Car{
+    Car(){
+        System.out.println("Car Object Created");
+    }
+    public static void main(String[] args){
+        Car c = new Car();    //constructor is called when object is created
+    }
+}
