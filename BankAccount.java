106d419a1790b994ceac12ca5857f4bdeb49f70c
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-04 15:02:02 +0530

    Bank management system

--- BankAccount.java
+++ BankAccount.java
@@ -1,19 +1,31 @@
-
-class BankAccount {
-
-    private int balance;
-
-    public void setBalance(int balance) {
+class BankAccount{
+    private String accountHolder;
+    private double balance;
+    BankAccount(String accountHolder, double balance){
+        this.accountHolder = accountHolder;
         this.balance = balance;
     }
-
-    public int getBalance() {
-        return balance;
+    void deposit(double amount){
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+        System.out.println("New Balance: " + balance);
+    }
+    void withdraw(double amount){
+        if(amount <= balance){
+            balance = balance - amount;
+            System.out.println("withdrawn: " + amount);
+            System.out.println("New Balance: " + balance);
+        }
+    }
+    void displayBalance(){
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Balance: " + balance);
     }
 
-    public static void main(String[] args) {
-        BankAccount ac = new BankAccount();
-        ac.setBalance(6000);
-        System.out.println("Balance: " + ac.getBalance());
+    public static void main(String[] args){
+        BankAccount ac = new BankAccount("shweta", 5000);
+        ac.deposit(2000);
+        ac.withdraw(1000);
+        ac.displayBalance();
     }
 }
