7b84b6c6b9c40aed495ca50596a1ab68eced82d7
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-04 13:49:04 +0530

    some changes

--- Animal2.java
+++ Animal2.java
@@ -3,12 +3,12 @@ class Animal2{
         System.out.println("Animal Sound");
     }
     public static void main(String[] args){
-        Dog d = new Dog();
+        Dog2 d = new Dog2();
         d.sound();
-        
+
     }
 }
-class Dog extends Animal2{
+class Dog2 extends Animal2{
     void sound(){
         super.sound();
         System.out.println("Dog Barking");
