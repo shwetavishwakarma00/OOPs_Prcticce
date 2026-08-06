2882eea11798b8b4dbe254189b003cc58fbe4a6d
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-06 13:03:36 +0530

    Question 6: Method Overriding - done

--- Main.java
+++ Main.java
@@ -1,17 +1,26 @@
 class Animal{
-    void eat(){
-        System.out.println("eating...");
+    void sound(){
+        System.out.println("Animal makes sound");
     }
 }
 class Dog extends Animal{
-    void bark(){
-        System.out.println("barking...");
+    
+    void sound(){
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal{
+     void sound(){
+        System.out.println("Cat meow");
     }
 }
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.sound();
         Dog d = new Dog();
-        d.bark();
-        d.eat();
+        d.sound();
+        Cat c = new Cat();
+        c.sound();
     }
 }
