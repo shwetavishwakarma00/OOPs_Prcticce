b43cd72973256aef106058b8377a26fad207ed98
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-06 17:26:11 +0530

    Question 7: Abstract Class

--- Shape.java
+++ Shape.java
@@ -1,15 +1,31 @@
 abstract class Shape{
-    abstract void draw();
-    public static void main(String[] args){
-        Shape s = new Circle();
-        s.draw();
-    }
+    int length;
+    int breadth;
+    double pi = 3.14;
+    int r;
+    abstract void area();
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.length = 10;
+        r.breadth = 12;
+        r.area();
+        Circle c = new Circle();
+        c.r = 7;
+        c.area();
 
-}
-class Circle extends Shape{
-    @Override
-    void draw(){
-        System.out.println("Drawing circle");
     }
+}
+class Rectangle extends Shape{
+  public void area(){
+    int react = length * breadth;
+    System.out.println("Reactangle : " + react);
+  }
+
+}   
+class Circle extends Shape{
+  public void area(){
+    double circle = pi * r * r;
+    System.out.println("Reactangle : " + circle);
+  }
 
 }
