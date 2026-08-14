38d715d9136ec37cf0ce2647009735f3cbb98c16
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2026-08-14 12:40:32 +0530

    BinarySearch

---
+++ DSA/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch{
+    static int binarySearch(int[] arr, int target){
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(target > arr[mid]){
+                low = mid + 1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return -1;      //it return not found elemnt message
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50,60,70}; 
+        int target = 50;
+        int result = binarySearch(arr, target);
+        if(result != -1){
+            System.out.println("Element found at index: " + result);
+        }else{
+            System.out.println("Element not found");
+        }
+    }
+}
